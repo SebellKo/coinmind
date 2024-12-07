dd29727bf4ae64e074acb6c4e4f806b23c11e1fd
SebellKo <79312827+SebellKo@users.noreply.github.com>
2024-12-08 00:16:52 +0900

    [feat]: 모든 post 데이터 가져오기 구현

--- models/postsData.js
+++ models/postsData.js
@@ -2,8 +2,8 @@ const { connectDB } = require('../db/db');
 
 const insertPostsData = async (postsData) => {
   try {
-    const db = (await connectDB).db(process.env.MONGODB_COLLECTION_NAME); // 데이터베이스 이름
-    const collection = db.collection('postsData'); // 컬렉션 이름
+    const db = (await connectDB).db(process.env.MONGODB_COLLECTION_NAME);
+    const collection = db.collection('postsData');
 
     const result = await collection.insertMany(postsData);
 
@@ -14,4 +14,18 @@ const insertPostsData = async (postsData) => {
   }
 };
 
-module.exports = { insertPostsData };
+const getAllData = async () => {
+  try {
+    const db = (await connectDB).db(process.env.MONGODB_COLLECTION_NAME);
+    const collection = db.collection('postsData');
+
+    const result = await collection.find().toArray();
+
+    return result;
+  } catch (error) {
+    console.log('get all data 오류', error);
+    throw error;
+  }
+};
+
+module.exports = { insertPostsData, getAllData };
