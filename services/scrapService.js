599195cdd6d011ca798a95d015b18cc7d28130c5
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-01-09 19:46:12 +0900

    [fix]: 스크래핑 차단 방지

--- services/scrapService.js
+++ services/scrapService.js
@@ -2,13 +2,26 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const scrapDC = async (page) => {
+  const userAgents = [
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36',
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36',
+    'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:90.0) Gecko/20100101 Firefox/90.0',
+  ];
+
+  const randomUserAgent =
+    userAgents[Math.floor(Math.random() * userAgents.length)];
+
+  const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+
+  await delay(1000);
+  console.log(`${page}번째 페이지 작업중`);
   try {
     const response = await axios.get(
       `https://gall.dcinside.com/board/lists/?id=bitcoins_new1&page=${page}`,
       {
         headers: {
-          'User-Agent':
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36',
+          'User-Agent': randomUserAgent,
+          timeout: 5000,
         },
       }
     );
@@ -39,7 +52,7 @@ const scrapDC = async (page) => {
 
     return headlines;
   } catch (error) {
-    console.error('크롤링 중 오류 발생:', error.message);
+    console.error(`page: ${page}, 크롤링 중 오류 발생:`, error.message);
     return [];
   }
 };
