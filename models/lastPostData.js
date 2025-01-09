7d0b1794b253f667963002710dc11948b8414ef5
SebellKo <79312827+SebellKo@users.noreply.github.com>
2025-01-09 19:46:46 +0900

    [fix]: 객체  프로퍼티 이름 변경

--- models/lastPostData.js
+++ models/lastPostData.js
@@ -11,7 +11,7 @@ const insertLastPostData = async (lastPostNum) => {
       { upsert: true }
     );
 
-    return result.value;
+    return result.lastPostNum;
   } catch (error) {
     console.error('lastPostData 삽입 에러', error);
     throw error;
